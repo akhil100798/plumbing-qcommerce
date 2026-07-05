7fe4cc0faf5c0c4859a550e5608c4b83a05d22bb
AKIII\akhil <hithisisakhilgmail.com>
2026-07-05 14:32:43 +0530

    Fix backend GitHub Actions CI

--- plumbing-qcommerce/backend/src/test/java/com/pqc/core/security/CorsConfigurationSecurityTest.java
+++ plumbing-qcommerce/backend/src/test/java/com/pqc/core/security/CorsConfigurationSecurityTest.java
@@ -13,7 +13,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles("local-staging")
+@ActiveProfiles({"local-staging", "test"})
 class CorsConfigurationSecurityTest {
 
     @Autowired
